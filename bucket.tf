13e88a3f8456237b7f0c39807110f48df41b0989
ballin24x7 <ballin24x7@gmail.com>
2023-01-10 14:58:48 -0600

    initial test

--- bucket.tf
+++ bucket.tf
@@ -1,8 +1,8 @@
-module "cloud-storage" {
-  source  = "terraform-google-modules/cloud-storage/google"
+module "cloud-storage_simple_bucket" {
+  source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version = "3.4.1"
   # insert the 3 required variables here
-  names = "file-storage"
-  prefix = "odi-gcp"
-  project_id = "odi-gcp"
+  location = "us-central1-c"
+  name = "odi-gcp-file-storage"
+  project_id = "odi-gcp"  
 }
--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ provider "google" {
   project = "odi-gcp"
   region  = "us-central1"
   zone    = "us-central1-c"
-  #credentials = file("~/gcp/odi-gcp-3d0b3c02ca72.json")
+  credentials = file("~/gcp/odi-gcp-3d0b3c02ca72.json")
 }
