c49e2bde422c6e2a06c4b74c8c7e8d5ae1320b93
OooohhhhYeeeaaaahhhh <72214649+OooohhhhYeeeaaaahhhh@users.noreply.github.com>
2023-01-10 16:29:58 -0600

    Delete bucket.tf

--- bucket.tf
+++
@@ -1,8 +0,0 @@
-module "cloud-storage_simple_bucket" {
-  source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
-  version = "3.4.1"
-  # insert the 3 required variables here
-  location = "us-central1-c"
-  name = "odi-gcp-file-storage"
-  project_id = "odi-gcp"  
-}
